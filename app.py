314863fe2b3142b8adfb10237d09d61ff94092e2
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-11-06 12:01:20 -0600

    Begins app code and makes a couple of empty routes for the home page, percipitation, stations and tobs.

---
+++ app.py
@@ -0,0 +1,27 @@
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+# 3. Define what to do when a user hits the index route
+@app.route("/")
+def home():
+    
+     
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
