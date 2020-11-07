9f6685b082cd3c6d71fe601584e46a3300c5b8cf
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-11-07 09:55:52 -0600

    adds sqlalchemy code and filles out percipitation page.

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+import numpy as np
 
-from flask import Flask
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
 
 # 3. Define what to do when a user hits the index route
 @app.route("/")
@@ -16,9 +27,20 @@ def home():
     
      
 
-#@app.route("/api/v1.0/precipitation")
-#def precipitation():
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    weather = session.query(Measurement.date, Measurement.prcp).group_by(Measurement.date).order_by(Measurement.date.desc()).limit(366)
+    session.close()
+    
+    percip={}
+    for date, prcp in weather:
+        
+        percip[date]=prcp
+        
 
+
+    return jsonify(percip)
 
 #@app.route("/api/v1.0/stations")
 #def stations():
