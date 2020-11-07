b1fcf9f6d6dcd09f08db88600d3baf01c73bcc5b
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-11-07 09:11:38 -0600

    Lists all routes on home page.

--- app.py
+++ app.py
@@ -8,19 +8,23 @@ app = Flask(__name__)
 # 3. Define what to do when a user hits the index route
 @app.route("/")
 def home():
+    return (f"Percipitation <br/>"
+    f"Stations <br/>"
+    f"tobs <br/>"
+    f"Start <br/>"
+    f"Start/end<br/>")
     
      
 
+#@app.route("/api/v1.0/precipitation")
+#def precipitation():
 
-@app.route("/api/v1.0/precipitation")
-def precipitation():
 
+#@app.route("/api/v1.0/stations")
+#def stations():
 
-@app.route("/api/v1.0/stations")
-def stations():
-    
-@app.route("/api/v1.0/tobs")
-def tobs():
+#@app.route("/api/v1.0/tobs")
+#def tobs():
 
 
 if __name__ == "__main__":
